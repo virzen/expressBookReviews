0042360e4f69abd9c3e7e9360cb2c6665611d5dc
Wiktor Czajkowski <wiktor.czajkowski@allegro.com>
2026-09-24 09:01:35 +0200

    public routes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -39,32 +39,54 @@ public_users.post("/register", (req, res) => {
 
 // Get the book list available in the shop
 public_users.get("/", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  res.json(books);
 });
 
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const { isbn } = req.params;
+
+  const book = books[isbn];
+
+  if (!book) {
+    res.status(404).send();
+    return;
+  }
+
+  res.json(book);
 });
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const { author } = req.params;
+  const booksList = Object.values(books);
+
+  const result = booksList.filter(b => b.author === author);
+
+  res.json(result);
 });
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const { title } = req.params;
+  const booksList = Object.values(books);
+
+  const result = booksList.filter(b => b.title === title);
+
+  res.json(result);
 });
 
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const { isbn } = req.params;
+  const book = books[isbn];
+
+  if (!book) {
+    res.status(404).send();
+    return;
+  }
+
+  res.json(book.reviews)
 });
 
 module.exports.general = public_users;
