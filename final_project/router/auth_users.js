c97e2789dfeb696c5e4bbae5915fe38a96811f00
Wiktor Czajkowski <wiktor.czajkowski@allegro.com>
2026-09-26 15:25:47 +0200

    put and delete review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -49,6 +49,7 @@ regd_users.post("/login", (req, res) => {
 
   const token = jwt.sign({ username: username }, SECRET, { expiresIn: 60 * 60 });
   req.session.token = token;
+  req.session.username = username;
 
   return res.status(200).json({ message: "User logged in successffully" });
 });
@@ -60,8 +61,39 @@ regd_users.post("/logout", (req, res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const { isbn } = req.params;
+  const book = books[isbn];
+  const content = req.body.content;
+  const username = req.session.username;
+
+  if (!book) {
+    res.status(400).send({ message: "Book not found" });
+    return;
+  }
+
+  if (!content) {
+    res.status(400).send({ message: ".content is required" });
+    return;
+  }
+
+  book.reviews[username] = content;
+
+  return res.status(200).json({ message: "Review added successfully" });
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const { isbn } = req.params;
+  const book = books[isbn];
+  const username = req.session.username;
+
+  if (!book) {
+    res.status(404).json({ message: "Book not found" });
+    return;
+  }
+
+  delete book.reviews[username];
+
+  return res.status(200).json({ message: "Review deleted successfully" });
 });
 
 module.exports.authenticated = regd_users;
